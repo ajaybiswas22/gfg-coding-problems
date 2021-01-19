9aa778b983b3b0814800107128af144db55d6c81
Ajay Biswas <ajaybiswas22@gmail.com>
2021-01-19 18:25:37 +0530

    Create 5_move_neg_side.cpp

---
+++ src/cpp/arrays/5_move_neg_side.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+using namespace std;
+
+void swap(int *xp, int *yp)  
+{  
+    int temp = *xp;  
+    *xp = *yp;  
+    *yp = temp;  
+} 
+
+void printArray(int arr[], int size)
+{
+   for (int i = 0; i < size; i++)
+   cout << arr[i] << " ";
+ 
+   cout << endl;
+} 
+
+void move_neg_side(int arr[],int n)
+{
+    // we need to pointers, one will be searching for negatives,
+    // other will be moving counter forward
+
+    int i=0,j=0;
+
+    while(i < n)
+    {
+        if(arr[i] < 0)
+        {
+            swap(arr[i],arr[j]);
+            j++;
+        }
+        i++;
+    }
+}
+
+int main()
+{
+
+    int n;
+    cin>>n;
+    int array[n];
+    for(int i = 0; i < n; i++)
+        cin>>array[i];
+
+    move_neg_side(array,n);
+    printArray(array,n);
+
+    return 0;
+}
