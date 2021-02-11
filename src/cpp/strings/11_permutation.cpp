e7454cb782a64ce4c496030f4bd6db27fe0a2a1b
Ajay Biswas <ajaybiswas22@gmail.com>
2021-02-11 20:18:47 +0530

    Create 11_permutation.cpp

---
+++ src/cpp/strings/11_permutation.cpp
@@ -0,0 +1,25 @@
+// program to find all permutation
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// permutation logic
+// fix one character, permute others
+// if two strings then swap
+// do with recursion
+
+void permutations(string s,int start,int end)
+{
+    
+    if(start == end)
+    {
+        
+    }
+}
+
+int main()
+{
+    string s = "AJAY";
+    permutations(s,0,3);
+
+}
