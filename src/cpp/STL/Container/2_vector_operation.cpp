8bfa175f13cedede7072698cea0ebc11ba90ad3e
Ajay Biswas <ajaybiswas22@gmail.com>
2021-03-02 23:42:53 +0530

    Update 2_vector_operation.cpp

--- src/cpp/STL/Container/2_vector_operation.cpp
+++ src/cpp/STL/Container/2_vector_operation.cpp
@@ -6,13 +6,12 @@ void printInt(vector<int> list)
     // range based for loop
     // Refer: https://en.cppreference.com/w/cpp/language/range-for
     for(int val: list)
-        cout<<val;
+        cout<<val<<" ";
     cout<<endl;
 }
 
 int main()
 {
-    
     vector<int> v;
     v.push_back(1);
     v.push_back(2);
@@ -21,9 +20,28 @@ int main()
     cout<<"At 0: "<<v.at(0)<<endl;          // element at index 0
     cout<<"Size: "<<v.size()<<endl;         // no. of elements  
     cout<<"Capacity: "<<v.capacity()<<endl; // capacity of the array
-
     v.clear();                              // removes all element
     cout<<"Size: "<<v.size()<<endl;
     cout<<"Capacity: "<<v.capacity()<<endl;
+    cout<<v.empty()<<endl;                  // is vector empty?
 
+    v.push_back(10);
+    v.push_back(20);
+    v.push_back(30);
+
+    v.erase(v.begin());                     // erase the first element
+    cout<<"erased"<<endl;
+    cout<<"Front: "<<v.front()<<endl;       // first value in the vector
+    cout<<"Back: "<<v.back()<<endl;         // last value in the vector
+    cout<<"Pop back:"<<endl;                
+    v.pop_back();                           // removed last element
+    printInt(v);
+
+    vector<int> v2 = {1,2,3};
+    v.swap(v2);                             // swap two vectors
+
+    cout<<"Vector v: ";
+    printInt(v);
+    cout<<"Vector v2: ";
+    printInt(v2);
 }
